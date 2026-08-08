ed363fc93a5e48c8c37597cedb4966746f337d20
Cobra546 <usmantabish26@gmail.com>
2026-08-09 02:49:36 +0500

    Update Supabase key in config.js
    Updated the Supabase key in the configuration.

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.PHOTO_BOOTH_CONFIG = {
   // Chapter 2 Supabase project
   SUPABASE_URL: 'https://bhtyestavehwaymfozxw.supabase.co',
   // Paste your Chapter 2 publishable/anon key below.
-  SUPABASE_KEY: 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE'
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJodHllc3RhdmVod2F5bWZvenh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NDkwODksImV4cCI6MjEwMTQyNTA4OX0.4-8U1fe5LQVTsuFa1kgc8A1PQeBQrh_UTibZsmApPrQ'
 };
