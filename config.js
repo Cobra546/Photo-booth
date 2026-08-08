3d041630509f2612aea20b69fd6ef65ed9ac6c1a
Cobra546 <usmantabish26@gmail.com>
2026-08-09 02:19:34 +0500

    Add client configuration template

---
+++ config.js
@@ -0,0 +1,5 @@
+/* Replace these two placeholders with the publishable values from your Supabase project. */
+window.PHOTO_BOOTH_CONFIG = {
+  SUPABASE_URL: 'YOUR_SUPABASE_PROJECT_URL',
+  SUPABASE_KEY: 'YOUR_SUPABASE_PUBLISHABLE_KEY'
+};
