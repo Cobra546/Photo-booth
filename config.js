913b72b697dc1237b652bca4bd6485abe67581f3
Cobra546 <usmantabish26@gmail.com>
2026-08-09 02:49:04 +0500

    Keep Supabase public key as deployment placeholder

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 window.PHOTO_BOOTH_CONFIG = {
   // Chapter 2 Supabase project
   SUPABASE_URL: 'https://bhtyestavehwaymfozxw.supabase.co',
-  // Add the Chapter 2 publishable/anon key here before deploying.
+  // Paste your Chapter 2 publishable/anon key below.
   SUPABASE_KEY: 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE'
 };
